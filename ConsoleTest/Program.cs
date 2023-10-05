cdf0fd81512d5b04329311cb470c5e777b0ae4a5
Layla <layla.shokri@gmx.de>
2023-10-05 15:14:52 +0200

    getcourses returns list of courses now

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using Moodle.Net.Extensions;
+
+namespace ConsoleTest;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var client = new Moodle.Net.Client("https://moodle.oszimt.de/", "9903806f77bc22af4fd0ed36d23e2ca9",
+            "webservice/rest/server.php");
+        Console.WriteLine(client.GetCourses().Courses[16].FullName);
+    }
+}
--- Moodle.Net/Extensions/CourseExtension.cs
+++ Moodle.Net/Extensions/CourseExtension.cs
@@ -1,13 +1,15 @@
-﻿using Moodle.Net.Models.Lists;
+﻿using Moodle.Net.Models;
+using Moodle.Net.Models.Lists;
 using Moodle.Net.Services;
 
 namespace Moodle.Net.Extensions;
 
 public static class ClientExtension
 {
-    public static CourseList GetCourses(this Client client)
+    public static List<Course> GetCourses(this Client client)
     {
         var courseService = new CourseService(client);
-        return courseService.GetCourses();
+        var response = courseService.GetCourses();
+        return response.Courses;
     }
 }
